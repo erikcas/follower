e6c2b7be0aa543465a6ddb0916300ff2383f4818
Erik Castricum <git@cas-online.nl>
2022-08-20 10:30:55 +0200

    followers: Make data retrieval optional
    while at it:
     * Correct typo
     * add requirements
    
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- followers.py
+++ followers.py
@@ -6,14 +6,32 @@ from sys import platform
 def analyse_user():
     print('Geef de twitter username (zonder @) in')
     print('en de datum vanaf wanneer je informatie wilt')
-    print('over volgers. Let op de aangegeven datum notatie.\n\n')
+    print('over volgers. Let op de aangegeven datum notatie.')
+    print('Geef ook aan of je bestaande data wilt gebruiken,')
+    print('je kunt ook de data opnieuw gebruiken door "n" in te geven.\n\n')
 
     wie = input('Twitter username: ')
     datum = input('Vanaf welke datum (dd-mm-jjj): ')
+    reuse_data = input('Data opnieuw ophalen (j|n): ')
+
+    if reuse_data == 'j' or reuse_data == 'J':
+        try:
+            if platform == 'linux' or platform == 'linux2' or platform == 'darwin':
+                os.system('rm *.json')
+            elif platform == 'win32':
+                os.system('del *.json')
+        except:
+            print('No files to delete')
+    elif reuse_data == 'n' or reuse_data == 'N':
+        print('We gebruiken de bestaande data indien aanwezig.')
+    else:
+        print('Graag j of n ingeven. Voer het script opnieuw uit.')
+        sys.exit()
+
     followers = get_follower_data(wie)
     test = check_tweeps(wie, datum)
 
-    if platform == 'linux' or platform == 'linux2' or platform = 'darwin':
+    if platform == 'linux' or platform == 'linux2' or platform == 'darwin':
         os.system('clear')
     elif platform == 'win32':
         os.system('cls')
@@ -21,13 +39,5 @@ def analyse_user():
     print(followers)
     print('\n\n')
     print(test)
-
-try:
-    if platform == 'linux' or platform == 'linux2' or platform = 'darwin':
-        os.system('rm *.json')
-    elif platform == 'win32':
-        os.system('del *.json')
-except:
-    print('No files to delete')
 
 analyse_user()
