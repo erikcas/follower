9afd0a11e48fae048cf4971e5b17bd0926891324
Erik Castricum <git@cas-online.nl>
2022-08-21 15:36:35 +0200

    followers: check: adjust date.
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- check.py
+++ check.py
@@ -6,7 +6,6 @@ import pandas as pd
 utc=pytz.UTC
 
 def check_tweeps(schermnaam, datum):
-    rij = 0
     # Maak leeg dataframe
     df = pd.DataFrame()
     #Twitter tijd is UTC dus even aanpassen
@@ -14,7 +13,9 @@ def check_tweeps(schermnaam, datum):
     startDate = utc.localize(datetime.strptime(datum, '%d-%m-%Y'))
     startDate = startDate - timedelta(hours=2)
     df = pd.read_json(f'{schermnaam}_followers.json')
-    df = df[['screen_name', 'id_str', 'created_at', 'followers_count', 'friends_count', 'statuses_count']]
+    # Zet de datum om, zodat we kunnen vergelijken
+    df['aanmaak'] = pd.to_datetime(df['created_at']).dt.date
+    df = df[['screen_name', 'id_str', 'aanmaak', 'followers_count', 'friends_count', 'statuses_count']]
     return df
 
 # Dit script vereist de tweepy en pytz module. Installeer:
