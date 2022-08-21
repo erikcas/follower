ded9b40d63a74840c10e6c454729d1e642c1ff75
Erik Castricum <git@cas-online.nl>
2022-08-21 13:34:31 +0200

    followers: simplify dataimport
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- check.py
+++ check.py
@@ -13,27 +13,8 @@ def check_tweeps(schermnaam, datum):
     twitter_datum = datetime.strptime(datum, '%d-%m-%Y').strftime('%Y-%m-%d')
     startDate = utc.localize(datetime.strptime(datum, '%d-%m-%Y'))
     startDate = startDate - timedelta(hours=2)
-    with open(f'{schermnaam}_followers.json', 'r') as f:
-        page = json.load(f)
-        for creep in page:
-            # Schermnaam
-            df.loc[rij, 'schermnaam'] = creep['screen_name']
-            # Twitter user id
-            df.loc[rij, 'user_id'] = creep['id_str']
-            # Wanneer is deze lid geworden van twitter
-            aanmaak = creep['created_at']
-            aanmaak = datetime.strftime(datetime.strptime(aanmaak,'%a %b %d %H:%M:%S +0000 %Y'), '%Y-%m-%d')
-            aanmaak = datetime.strptime(aanmaak, '%Y-%m-%d').strftime('%d-%m-%Y')
-            df.loc[rij, 'created'] = aanmaak
-            # Hoeveel volgers?
-            df.loc[rij, 'volgers'] = creep['followers_count']
-            # Hoeveel volgend?
-            df.loc[rij, 'volgend'] = creep['friends_count']
-            # Hoeveel tweets?
-            df.loc[rij, 'tweets'] = creep['statuses_count']
-            # De check. Pak alleen de tweeps lid geworden na de ingegeven datum
-            rij += 1
-
+    df = pd.read_json(f'{schermnaam}_followers.json')
+    df = df[['screen_name', 'id_str', 'created_at', 'followers_count', 'friends_count', 'statuses_count']]
     return df
 
 # Dit script vereist de tweepy en pytz module. Installeer:
